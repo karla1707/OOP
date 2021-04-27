cce27538436a873152f7fa4b46e6fce9536c69d9
karla1707 <54893296+karla1707@users.noreply.github.com>
2021-04-27 13:56:05 +0200

    Add files via upload

---
+++ OOP/ppar-2020-assignment-2-karla1707-master/PPARtask2/ConsoleApp/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal delegate void CommandMethod();
+
+    internal class Command
+    {
+        private CommandMethod methodToExecute;
+
+        public string Description { get; set; }
+
+        internal Command(CommandMethod commandMethod, string description)
+        {
+            methodToExecute = commandMethod;
+            Description = description;
+        }
+
+        internal void Execute()
+        {
+            methodToExecute();
+        }
+    }
+}
